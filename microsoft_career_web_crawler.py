5c08fc44e92c05c9e2db6c55675f68d17370ec5e
Michael Louie <michael.louie10@gmail.com>
2019-01-26 15:03:13 -0500

    Implemented job search keyword invitation interview day, added time diff, bug fixes, comments everywhere

--- microsoft_career_web_crawler.py
+++ microsoft_career_web_crawler.py
@@ -1,36 +1,33 @@
 """
     Web Crawler intended for Microsoft's Career Page
-    Crawls for all software engineer positions that are involved
-    with some invitational hiring event
+    Crawls for all entry-level software engineer positions
+    involved with some invitation-only hiring event
 """
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
 import json
+import datetime
 
-"""
-Experienced Professional = 'professional'
-Students and recent graduates = 'university'
-"""
-#role_type = 'professional'
-
-# Role sought after (e.g. software engineer)
-role_name = 'software engineer'
-
-def atoi(s):
+def atoi(s : str):
     """
         Converts integer jobId as string to integer
 
         Args:
             s (string): Microsoft jobId
+            
         Returns:
             integer form
     """
+    if not s:
+        print("Cannot convert empty string to integer")
+    
     n = 0
     for i in s:
         n = n * 10 + ord(i) - ord("0")
     return n
+
 
 def getListOfPositions(position_name : str):
     """
@@ -44,19 +41,22 @@ def getListOfPositions(position_name : str):
             List of positions based on position_name and other criteria defined
             in body variables as JSON.
 
-            Note: Actual JSON returned determined by endpoint
+        Note: Actual JSON returned determined by endpoint
     """
 
     if not position_name:
         print("Please specify a position_name")
         return None
 
+    # Microsoft careers endpoint
     url = 'https://careers.microsoft.com/widgets'
-    
+
+    # Specific JSON data is expected by endpoint
     data = {
 
-        # Edit these fields as you see fit
-        "searchType":"professional",
+        # Edit these fields as you see fit.
+        # They must conform to existing data fields found on career page
+        "searchType":"professional", #professional or university
         "keywords": position_name,
         "selected_fields": {
             "country": ["United States"],
@@ -72,12 +72,15 @@ def getListOfPositions(position_name : str):
         "ddoKey": "refineSearch",
         "sortBy": "Most relevant",
         "subsearch": "",
-        "from": 0,
+        "from": 0, # Return the first `from` to `size` results.
+                   # Change this field iteratively if displaying only smaller portions of `size` results
         "jobs": True,
         "counts": True,
         "all_fields": ["country","state","city","category","employmentType","requisitionRoleType","educationLevel"],
         "pageName": "search-results",
-        "size": 10000,
+        "size": 10000, # Use small numbers when testing - don't be a jerk to Microsoft.
+                       # Preferred max is 10000.
+                       # Note: This returns first `size` results that Microsoft returns
         "clearAll": False,
         "jdsource": "facets",
         "isSliderEnable": False,
@@ -96,42 +99,17 @@ def isMatchingJob(job : dict):
         Determines if Microsoft job matches criteria specified.
 
         Args:
-            job (list): A job
-                [0]     = "country"
-                [1]     = "subCategory"
-                [2]     = "industry"
-                [3]     = "title"
-                [4]     = "multi_location"
-                [5]     = "type"
-                [6]     = "orgFunction"
-                [7]     = "locale"
-                [8]     = "multi_location_array"
-                    [N] = "location"
-                [9]     = "jobSeqNo"
-                [10]    = "postedDate"
-                [11]    = "searchresults_display"
-                [12]    = "descriptionTeaser"
-                [13]    = "dateCreated"
-                [14]    = "state"
-                [15]    = "targetLevel"
-                [16]    = "jd_display"
-                [17]    = "reqId"
-                [18]    = "badge"
-                [19]    = "jobId"
-                [20]    = "isMultiLocation"
-                [21]    = "jobVisibility"
-                    [N] = "external"
-                [22]    = "mostpopular"
-                [21]    = "location"
-                [22]    = "category"
-                [23]    = "locationLatlong"
-        Returns true if matches some criteria, false otherwise
+            job (list): A job in JSON returned by Microsoft career endpoint
+                
+        Returns:
+            - true if matches some criteria
+            - false otherwise
     """
 
     '''
         Custom criteria
-            - Do not match a senior or specialized titular roles
-            - Consider only SDE I-II levels (59-62)
+            - Do not match senior or similar, specialized titular roles
+            - Consider only SDE I-II levels (59-62) (this is hidden to user)
     '''    
     try:
         titleSplit = job["title"].split()
@@ -141,7 +119,8 @@ def isMatchingJob(job : dict):
                         "Scientist", "SW", "HW", "Hardware", "SRE", "Lead",
                         "Site", "Reliability", "Test", "Cloud", "DCS",
                         "SW/FW", "Mobile", "Network", "Data", "International",
-                        "Quantum", "Machine", "UI", "Firmware")):
+                        "Quantum", "Machine", "UI", "UX", "Firmware",
+                        "High-Performance", "C++")):
                 return False
 
         targetLevel = atoi(job["targetLevel"])
@@ -149,8 +128,41 @@ def isMatchingJob(job : dict):
     except KeyError as ke:
         print("Error processing job with title: " + job["title"])
         return False
-    
 
+
+def isMatchingJobInterviewDay(job_url : str):
+    """
+        Determines if job description mentions anything pertaining to
+        an invitation-only interview day
+
+        Args:
+            job_url (string): URL to specific Microsoft position
+        
+    """
+    if not job_url:
+        print("Cannot check empty job_url")
+        return False
+    
+    try:
+        job_page = urlopen(job_specific_url)
+        soup = BeautifulSoup(job_page, 'html.parser')
+
+        # Microsoft hid job description information in JavaScript.
+        # Assuming something has to render it first to display to user.
+        # Search entire script value - very inefficient!
+        script = soup.find("script", type="text/javascript")        
+        return script.text.find("By applying to this position") != -1
+        # TODO: Enhance to ignore all past interview days
+    except:
+        # Frequent 502 errors occur, need some way to retry.
+        print("Error processing url: " + job_specific_url)
+        
+    return False
+
+beginTime = datetime.datetime.now()
+print("Starting web crawl:\t" + str(beginTime))
+
+role_name = 'software engineer'
 jobs_json = getListOfPositions(role_name)
 
 if not jobs_json:
@@ -164,19 +176,19 @@ for job in jobs:
     if isMatchingJob(job):
         matching_job_ids.append(job["jobId"])
         unique_titles_temp.add(job["title"])
-        
 
 job_specific_url_root = "https://careers.microsoft.com/us/en/job/"
 for job_id in matching_job_ids:
     job_specific_url = job_specific_url_root + job_id
-    job_page = urlopen(job_specific_url)
-    soup = BeautifulSoup(job_page, 'html.parser')
-    print(soup.prettify())
-    #print(soup.find('strong'))
-    #TODO: Parse for invitation day text
+    if isMatchingJobInterviewDay(job_specific_url):
+        print(job_specific_url)
 
-print("List of matched jobs")
+print("\nList of matched jobs")
 print("==========================")
 for title in unique_titles_temp:
     print(title)
-    
+
+endTime = datetime.datetime.now()
+print("\nEnding web crawl:\t" + str(endTime))
+print("\nTime elapsed: \t" + str(endTime - beginTime))
+              
